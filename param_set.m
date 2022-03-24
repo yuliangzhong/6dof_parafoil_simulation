095e1aecc1f2a4f6a997b44a4bba0f88d1553b6e
yuliangzhong <yubaby2003@163.com>
2022-03-24 19:21:53 +0100

    simulator verified! tested

--- param_set.m
+++ param_set.m
@@ -1,32 +1,33 @@
 %% Environment
 gravity_acc = + 9.81; % z-down
-laminar_wind_vel_at6m = 2; % absolute value
+laminar_wind_vel_at6m = 0; % absolute value
 laminar_wind_ori_at6m = 0; % 0-north, clockwise, degree
 turbulence_vel_at6m = 0; % absolute value
 turbulence_ori_at6m = 0; % 0-north, clockwise, degree
 
 %% Parafoil System
 system_mass = 2.2; % kg
-system_inertia =  [1.68, 0,    0.05;
+system_inertia =  [1.68, 0,    0.09;
                    0,    0.80, 0;
-                   0.05,0,    0.32];
+                   0.09, 0,    0.32];
 
 parafoil_ref_area = 1.5;  % m2
 parafoil_ref_span = 1.88; % m
-parafoil_ref_length = 0.8; % m, chord
+parafoil_ref_length = 0.80; % m, chord
 parafoil_arc_h = 0.1; % m
-parafoil_thickness = 0.03; % m, thickness of canopy
+parafoil_thickness = 0.075; % m, thickness of canopy
 
 S_pd = 0.06; % payload reference area
 
 C_BP = eye(3); % from body frame B to parafoil aero frame P;
-B_r_BP = [0.2; 0; -1.2];
+B_r_BP = [0; 0; -1.0];
 
 C_BW = eye(3); % from body frame B to payload frame W;
-B_r_BW = [0; 0; 0.15];
+B_r_BW = [0; 0; 0.1];
 
 [Im, Ii] = ImIiCompute(parafoil_arc_h, parafoil_ref_span, ...
                     parafoil_ref_length, parafoil_thickness);
+
 
 %% Aero Coefficients
 c_L0 = 0.24; % lift force coefficient
@@ -39,17 +40,17 @@ c_DA2 = 0.33;
 c_DBs = 0.43;
 c_DBa = 0.0957;
 
-c_Yb = 1.0;
+c_Yb = 0.23;
 
-c_lp = -0.84;
+c_lp = -0.84; % negative
 c_lBa = 0.005;
-c_m0 = 0.35;
-c_mA = -0.72;
-c_mq = -1.49;
-c_nr = -0.27;
+c_m0 = -0.05; % highly influence alpha! compensate apparent mass moment in Y
+c_mA = -0.70; % highly influence alpha! compensate apparent mass moment in Y
+c_mq = -1.49; % negative
+c_nr = -0.27; % negative
 c_nBa = 0.0115;
 
-c_Dpd = 0.1; % payload drag coefficient
+c_Dpd = 0.5; % payload drag coefficient
 
 cL = [c_L0; c_LA; c_LBs; c_LBa]; % lift force coefficients
 cD = [c_D0; c_DA2; c_DBs; c_DBa]; % drag force coefficients
@@ -57,8 +58,8 @@ cM = [c_lp; c_lBa; c_m0; c_mA; c_mq; c_nr; c_nBa]; % moment coefficients
 
 %% Initiation
 init_pos_in_inertial_frame = [0, 0, -250]; % x-desired pose, z-down
-init_rpy = [0,  0,  0]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
-init_uvw = [2, 0,  0]; % velocity in body frame
+init_rpy = [0,      0.002,  0]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
+init_uvw = [5.61,   0,      1.25]; % velocity in body frame
 init_pqr = [0,  0,  0]; % angular velocity in body frame
 
 %% useful function
