3bbfb8c1a55bc99b493a6bf3e081cf7f036c4e8a
yuliangzhong <yubaby2003@163.com>
2022-03-28 23:32:08 +0200

    tried aerodynamic coef estimation, bug exists

--- param_set.m
+++ param_set.m
@@ -1,9 +1,9 @@
 %% Environment
 gravity_acc = + 9.81; % z-down
 laminar_wind_vel_at6m = 1; % absolute value
-laminar_wind_ori_at6m = 45; % 0-north, clockwise, degree
+laminar_wind_ori_at6m = 0; % 0-north, clockwise, degree
 turbulence_vel_at6m = 0.5; % absolute value
-turbulence_ori_at6m = 60; % 0-north, clockwise, degree
+turbulence_ori_at6m = 0; % 0-north, clockwise, degree
 
 %% Parafoil System
 system_mass = 2.2; % kg
@@ -42,9 +42,9 @@ c_Yb = 0.23;
 
 c_lp = -0.84; % negative
 c_lBa = -0.005; % negative
-c_m0 = -0.05; % highly influence alpha! compensate apparent mass moment in Y
-c_mA = -0.70; % highly influence alpha! compensate apparent mass moment in Y
-% c_m0 = 0; c_mA = 0;
+% c_m0 = -0.05; % highly influence alpha! compensate apparent mass moment in Y
+% c_mA = -0.70; % highly influence alpha! compensate apparent mass moment in Y
+c_m0 = 0; c_mA = 0;
 
 c_mq = -1.49; % negative
 c_nr = -0.27; % negative
@@ -57,15 +57,15 @@ cD = [c_D0; c_DA2; c_DBs]; % drag force coefficients
 cM = [c_lp; c_lBa; c_m0; c_mA; c_mq; c_nr; c_nBa]; % moment coefficients
 
 %% Initiation
-init_pos_in_inertial_frame = [-400, -400, -250]; % x-desired heading, z-down
-init_rpy = [0,      0.002,  pi/2]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
-init_uvw = [5.62,   0,      1.25]; % velocity in body frame
-init_pqr = [0,  0,  0]; % angular velocity in body frame
-
-% init_pos_in_inertial_frame = [-400, -400, -250]; % x-desired heading, z-down
-% init_rpy = [0,      0.27,  pi/2]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
-% init_uvw = [4.05,   0,      1.91]; % velocity in body frame
+% init_pos_in_inertial_frame = [-400, -400, -250]; % x-North, z-down, y-East
+% init_rpy = [0,      0,  pi/2]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
+% init_uvw = [5.61,   0,  1.25]; % velocity in body frame
 % init_pqr = [0,  0,  0]; % angular velocity in body frame
+
+init_pos_in_inertial_frame = [-400, -400, -250]; % x-North, z-down, y-East
+init_rpy = [0,      0.27,  pi/2]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
+init_uvw = [4.05,   0,      1.90]; % velocity in body frame
+init_pqr = [0,  0,  0]; % angular velocity in body frame
 
 %% Observer accuracy(accu)
 sampling_T = 0.5; % [seconds]
@@ -75,28 +75,26 @@ pos_accu = 1; % [m]
 vel_accu = 0.05; % [m/s]
 acc_accu = 0.5; % [m/s^2]
 angVel_accu = 0.02; %[deg/s]
-airspeed_var = [0.001, 0.001, 0.001]; % [alpha, beta, Vb], no unit
+airspeed_var = [0.001, 0.001, 0.005]; % [alpha, beta, Vb], no unit
+                                      % about [2.3deg, 2.3deg, 0.05m/s]
 
 %% Wind Estimator
 mu0 = -[laminar_wind_vel_at6m*cos(laminar_wind_ori_at6m/180*pi); 
        laminar_wind_vel_at6m*sin(laminar_wind_ori_at6m/180*pi);
-       0];
-sigma0 = eye(3);
-wind_est_noise_var = 0.5*eye(3); % d ~ N(0, R), R matrix
+       0]; % wind mean initial guess
+sigma0 = eye(3); % wind variance initial guess
+wind_est_noise_var = 0.5*eye(3); % d ~ N(0, R), R matrix, sensor noise
 
-%% useful function
-function [Im, Ii] = ImIiCompute(a, b, c, t)
+%% Aerodynamic Coefficients Estimator
+aeroF_co_mu0 = [0 0 0; 
+                0 0 0; 
+                0 0 0;
+                0 0 0]; % [cD; cYb; cL; cDpd], delta_s = 0; 0.5; 1;
+aeroF_co_sigma0 = 0.05*[eye(4), eye(4), eye(4)];
+aeroF_est_noise_var = 0.05*eye(3);
 
-    a_bar = a/b;
-    t_bar = t/c;
-    AR = b/c;
-    A = 0.666 * (1 + 8/3*a_bar^2) * t^2 * b;
-    B = 0.267 * (t^2 + 2*a^2*(1-t_bar^2))*c;
-    C = 0.785 * sqrt(1 + 2*a_bar^2*(1-t_bar^2))*AR/(1+AR)*b*c^2;
-    D = 0.055 * AR/(1+AR)*b^3*c^2;
-    E = 0.0308 * AR/(1+AR)*(1 + pi/6*(1+AR)*AR*a_bar^2*t_bar^2)*b*c^4;
-    F = 0.0555 * (1+8*a_bar^2)*t^2*b^3;
-    Im = diag([A, B, C]);
-    Ii = diag([D, E, F]);
 
-end
+
+
+
+
