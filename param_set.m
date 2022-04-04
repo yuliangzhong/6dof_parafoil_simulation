e7e3f81c49e492ebe1b66d426215d6a5fbca72a2
yuliangzhong <yubaby2003@163.com>
2022-04-04 15:12:42 +0200

    tested

--- param_set.m
+++ param_set.m
@@ -7,12 +7,10 @@ wind_h = @(h) vel_at6m*log(h/0.04572)/log(6.096/0.04572); % wind shear model
 theta_h = @(h) - 5/6*pi + 1/6*pi * h/300; % forcasted wind field
 GetWindProfile = @(h) [wind_h(h).*cos(theta_h(h));
                        wind_h(h).*sin(theta_h(h));
-                       zeros(1,size(h,2))];
+                       zeros(1,size(h,2))]; % [wx; wy; 0];
 heights = linspace(0.05, 300, 3000);
 wind_profile_hat = GetWindProfile(heights);
-xi_w = 0.4* [randn();
-             randn();
-             randn()]; % xi_w(h) = xi_w * h / 100
+xi_w = 0.4* randn(3,1); % xi_w(h) = xi_w * h / 100
 a_w = -0.00385;
 b_w = 0.0251; % params from paper 14/16
 
@@ -85,10 +83,10 @@ airspeed_var = [0.001, 0.001, 0.005]; % [alpha, beta, Vb], no unit
                                       % about [2.3deg, 2.3deg, 0.05m/s]
 
 %% Wind Estimator
-mu0 = WindForcast(-init_pos_in_inertial_frame(3),0);
+mu0 = GetWindProfile(-init_pos_in_inertial_frame(3));
 sigma0 = eye(3); % wind variance initial guess
 w_bar_hat0 = mu0;
-wind_est_dyn_var = 1.01* sampling_T * b_w * eye(3); % v ~ N(0, Q), Q matrix
+wind_est_dyn_var = 1.1* sampling_T * b_w * eye(3); % v ~ N(0, Q), Q matrix
 wind_est_noise_var = 0.5*eye(3); % d ~ N(0, R), R matrix, sensor noise
 
 %% Aerodynamic Coefficients Estimator
