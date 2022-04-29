aad1e09b9b41589ff5c2dd0a591419a0073cd37f
yuliangzhong <yubaby2003@163.com>
2022-04-29 15:49:58 +0200

    feature: MPC 2d path following works! debugging

--- MPC_2d_path_following_func.m
+++ MPC_2d_path_following_func.m
@@ -1,4 +1,6 @@
-function [flag, xs, us] = MPC_2d_path_following_func(x0, y0, h0, psi0, guidance_in_wind)
+function [flag, xs, us, Dh] = MPC_2d_path_following_func(x0, y0, h0, psi0, guidance_in_wind)
+
+% warning('off','MATLAB:polyfit:RepeatedPointsOrRescale')
 
 % Params
 psi_dot_max = 0.2187;
@@ -6,69 +8,57 @@ xy_dot = 4.59;
 z_dot = 1.39;
 um = psi_dot_max;
 
-[~, id] = min(abs(guidance_in_wind(3,:) - h0*ones(1,2000)));
-xd = guidance_in_wind(1,id);
-yd = guidance_in_wind(2,id);
-hd = guidance_in_wind(3,id);
-psid = guidance_in_wind(4,id);
 
-disp([x0, y0, h0, psi0])
-disp([xd, yd, hd, psid])
+
+[~, id] = min(vecnorm(guidance_in_wind(1:2,:) - [x0; y0]*ones(1,2000)));
+hd = guidance_in_wind(3,id);
+Dh = h0 - hd;
+disp(guidance_in_wind(:,id)') 
 
 % fitting --> fx, fy, fpsi
 N = 100;
+ids = max(id - 50, 1);
 idn = min(id+2*N, 2000);
 
-% thetas = guidance_in_wind(3,id:idn);
-% x_s = guidance_in_wind(1,id:idn);
-% y_s = guidance_in_wind(2,id:idn);
-% psi_s = guidance_in_wind(4,id:idn);
-
-px = polyfit(guidance_in_wind(3,id:idn), guidance_in_wind(1,id:idn), 7);
+px = polyfit(guidance_in_wind(3,ids:idn), guidance_in_wind(1,ids:idn), 7);
 fx = @(x) px*[x.^7; x.^6; x.^5; x.^4; x.^3; x.^2; x; ones(1,size(x,2))];
-py = polyfit(guidance_in_wind(3,id:idn), guidance_in_wind(2,id:idn), 7);
+py = polyfit(guidance_in_wind(3,ids:idn), guidance_in_wind(2,ids:idn), 7);
 fy = @(x) py*[x.^7; x.^6; x.^5; x.^4; x.^3; x.^2; x; ones(1,size(x,2))];
-ppsi = polyfit(guidance_in_wind(3,id:idn), guidance_in_wind(4,id:idn), 7);
+ppsi = polyfit(guidance_in_wind(3,ids:idn), guidance_in_wind(4,ids:idn), 7);
 fpsi = @(x) ppsi*[x.^7; x.^6; x.^5; x.^4; x.^3; x.^2; x; ones(1,size(x,2))];
-
-% y1 = fx(thetas);
-% y2 = fy(thetas);
-% y3 = fpsi(thetas);
-% 
-% plot(thetas,x_s)
-% hold on
-% plot(thetas,y_s)
-% plot(thetas, psi_s)
-% plot(thetas,y1)
-% plot(thetas,y2)
-% plot(thetas,y3)
-% hold off
 
 % MPC formulation
 Ts = 0.1;
 
-
-P = diag([100,100,10000]);
-Q = diag([0, 0, 10000]);
-R = [1000, 1000000];
+% P = diag([100,100,10000]);
+Q = diag([1000, 1000]);
+R = diag([1000, 1000]);
+r = 100000;
+q_theta = 100;
 
 Prob = casadi.Opti();
 % --- define optimization variables ---
-X = Prob.variable(4, N+1);
-U = Prob.variable(2, N);
+X = Prob.variable(4, N+1); % [x, y, psi, theta]
+U = Prob.variable(2, N);   % [u, v]
 
 % --- calculate objective --- 
 objective = 0;
 
-for i = 1:N
-    objective = objective + (X(1:2,i) - [fx(X(4,i)); fy(X(4,i))])'* Q(1:2, 1:2) *(X(1:2,i) - [fx(X(4,i)); fy(X(4,i))]) ...
-                          + Q(3,3) * (1 - cos(X(3,i) - fpsi(X(4,i)))) ...
-                          + R(1) * U(1,i)^2 ...
-                          + R(2) * (U(2,i) - z_dot)^2;
+for i = 2:N
+    psi = fpsi(X(4,i));
+    es_l = -cos(psi)*(X(1,i) - fx(X(4,i))) - sin(psi)*(X(2,i) - fy(X(4,i)));
+    es_c = sin(psi)*(X(1,i) - fx(X(4,i))) - cos(psi)*(X(2,i) - fy(X(4,i)));
+    du = U(1,i) - U(1,i-1);
+    dv = U(2,i) - U(2,i-1);
+    objective = objective + [es_c, es_l]* Q * [es_c, es_l]' ...
+                          + q_theta * X(4,i) ...
+                          + r*U(1,i)^2 ...
+                          + [du, dv]* R *[du, dv]';
 end
-objective = objective + (X(1:2,N+1) - [fx(X(4,N+1)); fy(X(4,N+1))])'* P(1:2, 1:2) *(X(1:2,N+1) - [fx(X(4,N+1)); fy(X(4,N+1))]) ...
-                      + P(3,3) *(1 - cos(X(3,N+1) - fpsi(X(4,N+1))));
-
+psi = fpsi(X(4,N+1));
+es_l = -cos(psi)*(X(1,N+1) - fx(X(4,N+1))) - sin(psi)*(X(2,N+1) - fy(X(4,N+1)));
+es_c = sin(psi)*(X(1,N+1) - fx(X(4,N+1))) - cos(psi)*(X(2,N+1) - fy(X(4,N+1)));
+objective = objective + [es_c, es_l]* Q * [es_c, es_l]';
 Prob.minimize(objective)
 
 % --- define constraints ---
@@ -98,5 +88,23 @@ else
     xs = zeros(4,N+1);
     us = zeros(2,N);
 end
+
+% % plot
+% hold on
+% grid on
+plot(guidance_in_wind(2,ids:idn),guidance_in_wind(1,ids:idn), 'g')
+% 
+% % states = [x0; y0; psi0; hd] * ones(1,N+1);
+% % for i = 1:N
+% %     states(:,i+1) = states(:,i) + Ts* [xy_dot * cos(xs(3,i));
+% %                                        xy_dot * sin(xs(3,i));
+% %                                        us(1,i);
+% %                                        -us(2,i)];
+% % end
+% % 
+scatter(fy(xs(4,:)), fx(xs(4,:)), 'r')
+plot(xs(2,:),xs(1,:), 'b')
+% axis equal
+% % hold off
 
 end
--- MPC_path_following.m
+++ MPC_path_following.m
@@ -1,3 +1,5 @@
+% warning('off','MATLAB:polyfit:RepeatedPointsOrRescale')
+figure;
 % Params
 psi_dot_max = 0.2187;
 xy_dot = 4.59;
@@ -9,76 +11,64 @@ guidance_in_wind = out.guidance_in_wind.signals.values(:,:,end);
 wind_err = out.wind_err.signals.values(:,:,end);
 C_BI = out.C_BI.signals.values(:,:,end);
 I_r_IB = out.I_r_IB.signals.values(end,:);
-
-h = -I_r_IB(3);
-x0 = I_r_IB(1) + (1/z_dot)*interp1(heights, Delta_s(1,:), h,'linear','extrap');
-y0 = I_r_IB(2) + (1/z_dot)*interp1(heights, Delta_s(2,:), h,'linear','extrap');
+h0 = -I_r_IB(3);
+x0 = I_r_IB(1) + (1/z_dot)*interp1(heights, Delta_s(1,:), h0,'linear','extrap');
+y0 = I_r_IB(2) + (1/z_dot)*interp1(heights, Delta_s(2,:), h0,'linear','extrap');
 C_IB = C_BI';
 psi0 = atan2(C_IB(2,1), C_IB(1,1));
+disp([x0, y0, h0, psi0])
 
-[~, id] = min(abs(guidance_in_wind(3,:) - h*ones(1,2000)));
-xd = guidance_in_wind(1,id);
-yd = guidance_in_wind(2,id);
+[~, id] = min(vecnorm(guidance_in_wind(1:2,:) - [x0; y0]*ones(1,2000)));
 hd = guidance_in_wind(3,id);
-psid = guidance_in_wind(4,id);
-
-disp([x0, y0, h, psi0])
-disp([xd, yd, hd, psid])
+Dh = h0 - hd;
+hold on
+disp(guidance_in_wind(:,id)') 
+scatter(guidance_in_wind(2,id), guidance_in_wind(1,id),'k')
 
 % fitting --> fx, fy, fpsi
 N = 100;
+ids = max(id - 50, 1);
 idn = min(id+2*N, 2000);
 
-% thetas = guidance_in_wind(3,id:idn);
-% x_s = guidance_in_wind(1,id:idn);
-% y_s = guidance_in_wind(2,id:idn);
-% psi_s = guidance_in_wind(4,id:idn);
-
-px = polyfit(guidance_in_wind(3,id:idn), guidance_in_wind(1,id:idn), 7);
+px = polyfit(guidance_in_wind(3,ids:idn), guidance_in_wind(1,ids:idn), 7);
 fx = @(x) px*[x.^7; x.^6; x.^5; x.^4; x.^3; x.^2; x; ones(1,size(x,2))];
-py = polyfit(guidance_in_wind(3,id:idn), guidance_in_wind(2,id:idn), 7);
+py = polyfit(guidance_in_wind(3,ids:idn), guidance_in_wind(2,ids:idn), 7);
 fy = @(x) py*[x.^7; x.^6; x.^5; x.^4; x.^3; x.^2; x; ones(1,size(x,2))];
-ppsi = polyfit(guidance_in_wind(3,id:idn), guidance_in_wind(4,id:idn), 7);
+ppsi = polyfit(guidance_in_wind(3,ids:idn), guidance_in_wind(4,ids:idn), 7);
 fpsi = @(x) ppsi*[x.^7; x.^6; x.^5; x.^4; x.^3; x.^2; x; ones(1,size(x,2))];
-
-% y1 = fx(thetas);
-% y2 = fy(thetas);
-% y3 = fpsi(thetas);
-% 
-% plot(thetas,x_s)
-% hold on
-% plot(thetas,y_s)
-% plot(thetas, psi_s)
-% plot(thetas,y1)
-% plot(thetas,y2)
-% plot(thetas,y3)
-% hold off
 
 % MPC formulation
 Ts = 0.1;
 
-
-P = diag([100,100,10000]);
-Q = diag([0, 0, 10000]);
-R = [1000, 1000000];
+% P = diag([100,100,10000]);
+Q = diag([1000, 1000]);
+R = diag([1000, 1000]);
+r = 100000;
+q_theta = 100;
 
 Prob = casadi.Opti();
 % --- define optimization variables ---
-X = Prob.variable(4, N+1);
-U = Prob.variable(2, N);
+X = Prob.variable(4, N+1); % [x, y, psi, theta]
+U = Prob.variable(2, N);   % [u, v]
 
 % --- calculate objective --- 
 objective = 0;
 
-for i = 1:N
-    objective = objective + (X(1:2,i) - [fx(X(4,i)); fy(X(4,i))])'* Q(1:2, 1:2) *(X(1:2,i) - [fx(X(4,i)); fy(X(4,i))]) ...
-                          + Q(3,3) * (1 - cos(X(3,i) - fpsi(X(4,i)))) ...
-                          + R(1) * U(1,i)^2 ...
-                          + R(2) * (U(2,i) - z_dot)^2;
+for i = 2:N
+    psi = fpsi(X(4,i));
+    es_l = -cos(psi)*(X(1,i) - fx(X(4,i))) - sin(psi)*(X(2,i) - fy(X(4,i)));
+    es_c = sin(psi)*(X(1,i) - fx(X(4,i))) - cos(psi)*(X(2,i) - fy(X(4,i)));
+    du = U(1,i) - U(1,i-1);
+    dv = U(2,i) - U(2,i-1);
+    objective = objective + [es_c, es_l]* Q * [es_c, es_l]' ...
+                          + q_theta * X(4,i) ...
+                          + r*U(1,i)^2 ...
+                          + [du, dv]* R *[du, dv]';
 end
-objective = objective + (X(1:2,N+1) - [fx(X(4,N+1)); fy(X(4,N+1))])'* P(1:2, 1:2) *(X(1:2,N+1) - [fx(X(4,N+1)); fy(X(4,N+1))]) ...
-                      + P(3,3) *(1 - cos(X(3,N+1) - fpsi(X(4,N+1))));
-
+psi = fpsi(X(4,N+1));
+es_l = -cos(psi)*(X(1,N+1) - fx(X(4,N+1))) - sin(psi)*(X(2,N+1) - fy(X(4,N+1)));
+es_c = sin(psi)*(X(1,N+1) - fx(X(4,N+1))) - cos(psi)*(X(2,N+1) - fy(X(4,N+1)));
+objective = objective + [es_c, es_l]* Q * [es_c, es_l]';
 Prob.minimize(objective)
 
 % --- define constraints ---
@@ -105,21 +95,24 @@ if flag
     us = sol.value(U);
 else
     disp("MPC Solution not found")
-    xs = zeros(3,N);
-    us = zeros(1,N-1);
-end
-% 
-% % plot
-hold on
-grid on
-plot(guidance_in_wind(2,id:idn),guidance_in_wind(1,id:idn), 'b')
-states = [x0; y0; psi0; hd] * ones(1,N+1);
-for i = 1:N
-    states(:,i+1) = states(:,i) + Ts* [xy_dot * cos(xs(3,i));
-                                       xy_dot * sin(xs(3,i));
-                                       us(1,i);
-                                       -us(2,i)];
+    xs = zeros(4,N+1);
+    us = zeros(2,N);
 end
 
-plot(states(2,:),states(1,:), 'k')
-hold off
+% plot
+hold on
+grid on
+plot(guidance_in_wind(2,ids:idn),guidance_in_wind(1,ids:idn), 'g')
+
+% states = [x0; y0; psi0; hd] * ones(1,N+1);
+% for i = 1:N
+%     states(:,i+1) = states(:,i) + Ts* [xy_dot * cos(xs(3,i));
+%                                        xy_dot * sin(xs(3,i));
+%                                        us(1,i);
+%                                        -us(2,i)];
+% end
+% 
+scatter(fy(xs(4,:)), fx(xs(4,:)), 'r')
+plot(xs(2,:),xs(1,:), 'b')
+axis equal
+% hold off
