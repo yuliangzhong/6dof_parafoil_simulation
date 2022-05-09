8fbd810e0a69bf46835f9e19fd63d61c21e759bc
yuliangzhong <yubaby2003@163.com>
2022-05-09 13:06:34 +0200

    why solving is so slow?

--- SCP_guidance.m
+++ SCP_guidance.m
@@ -1,47 +1,27 @@
-% SCP Guidance
-clear all;
-clc;
+function SCP_guidance(N, sys_param0, init_cond, psi_d, heights, wind_profile_hat)
 
-%% Wind Profile, Pos Compensation and Wind Gust Dynamics
-vel_at6m = 1.5; % wind velocity at 6m, absolute value
-theta = 120; % [deg] constant
-
-wind_h = @(h) (h>0.04572)*vel_at6m.*log(h/0.04572)/log(6.096/0.04572); % wind shear model
-theta_h = @(h) theta/180*pi + pi; % forcasted wind field
-GetWindProfile = @(h) [wind_h(h).*cos(theta_h(h));
-                       wind_h(h).*sin(theta_h(h));
-                       zeros(1,size(h,2))]; % [wx; wy; 0];
-wind_pf_size = 7000;
-heights = linspace(1e-6, 350, wind_pf_size); % start from 0+ avoiding NaN
-wind_profile_hat = GetWindProfile(heights);
-
-%% Parameters
-N = 100;
-psi_dot_m = 0.2187;
-h0 = 262;
-Vh0 = 4.59;
-Vz0 = 1.39;
-x0 = 200;
-y0 = -200;
-psi_0 = pi*2/4;
-psi_d = pi;
-% wind_profile_hat
-% heights
+%% Parameters Reading
+Vh0 = sys_param0(1);
+Vz0 = sys_param0(2);
+psi_dot_m = sys_param0(3);
+x0 = init_cond(1);
+y0 = init_cond(2);
+h0 = init_cond(3);
+psi_0 = init_cond(4);
 
 %% Altitude Evolution
-
 ch = 1.225;
 cz = 2.256e-5;
 ce = 4.2559;
 cf = ce/2+1;
-
+% functions
 rho = @(h) ch*(1-h*cz).^ce;
 h = @(t) 1/cz*(ones(1,size(t,2)) - (cz*cf*Vz0*sqrt(rho(h0))/sqrt(ch)*t + (1-cz*h0)^cf*ones(1,size(t,2))).^(1/cf));
 Vh = @(h) Vh0*sqrt(rho(h0)./rho(h));
 
 tf = sqrt(ch)/(cz*cf*Vz0*sqrt(rho(h0)))*(1-(1-cz*h0)^cf);
 dt = tf/N;
-
+% time mesh
 times = linspace(0, tf, N+1);
 hs = h(times);
 Vhs = Vh(hs);
@@ -54,16 +34,15 @@ u0 = [Vh0*cos(psi_0);
       Vh0*sin(psi_0)];
 u_bar_normalized = u0/norm(u0)*ones(1,N+1);
 
-eps_h = 0.1;
+eps_h = 0.05;
 MAX_ITER = 50;
 current_cost = inf;
 eps_convergence = 0.1;
 lambda1 = 100;
 lambda2 = 10;
-lambda3 = 1;
+lambda3 = 100;
 
-x_star = zeros(2,N+1);
-u_star = zeros(2,N+1);
+% guidance = zeros(5,N);
 
 %% Optimization Stage One
 
@@ -79,33 +58,121 @@ for i = 1:N
                                                          (u(2,i)+u(2,i+1))/2 + (Ws(2,i)+Ws(2,i+1))/2] ...
                               , norm(u(:,i)) <= Vhs(i) + eps_h ...
                               , norm(u(:,i+1) - u(:,i))/Vhs(i)/dt <= psi_dot_m];
-    cost = cost + lambda3*norm(u(:,i+1) - u(:,i))^2/Vhs(i)^2/dt;
+    cost = cost + norm(u(:,i+1) - u(:,i))^2/Vhs(i)^2/dt;
 end
 options = sdpsettings('verbose',0,'solver','ecos');
 
-for n = 1:MAX_ITER
-    disp("At iteration "+num2str(n)+", current cost: "+num2str(current_cost))
+figure(1)
+hold on
+grid on
 
+for n = 1:MAX_ITER
     Cons = constraints;
     for i = 1:N+1
         Cons = [Cons, u_bar_normalized(:,i)'*u(:,i) >= Vhs(i) - eps_h];
     end
     sol = optimize(Cons, cost, options);
     if sol.problem ~= 1
-        if abs(value(cost) - current_cost) < eps_convergence
-            x_star = value(x);
-            u_star = value(u);
+        if abs(value(cost) - current_cost) < eps_convergence && value(cost)<5
+            xs = value(x);
+            us = value(u);
+            for j = 1:N+1
+                u_bar_normalized(:,j) = us(:,j)/norm(us(:,j));
+            end
+%             guidance(1:2,:) = xs(:,2:end);
+%             guidance(3,:) = hs(2:end);
+%             guidance(4,:) = atan2(us(2,2:end), us(1,2:end));
+%             guidance(5,:) = 
+% 
+%             us = value(u);
+%             flag = true;
+
             disp("At iteration "+num2str(n)+", final cost: "+num2str(value(cost)))
+            disp([lambda1*(xs(1,end)^2 + xs(2,end)^2), lambda2 * (1-cos(psi_d)*us(1,end)/Vhs(end) - sin(psi_d)*us(2,end)/Vhs(end))])
+            plot(xs(2,:), xs(1,:),'r')
             break
         else
             current_cost = value(cost);
+            disp("At iteration "+num2str(n)+", current cost: "+num2str(current_cost))
+            xs = value(x);
+            plot(xs(2,:), xs(1,:),'k')
             for j = 1:N+1
-                u_sol = value(u);
-                u_bar_normalized(:,j) = u_sol(:,j)/norm(u_sol(:,j));
+                us = value(u);
+                u_bar_normalized(:,j) = us(:,j)/norm(us(:,j));
             end
         end
     else
-        disp("Something went wrong at iteration "+num2str(n));
+        disp("###############################################")
+        disp("[ERROR] Something went wrong at iteration "+num2str(n));
         disp(sol.info)
+        disp("###############################################")
     end
 end 
+
+%% Optimization Stage Two
+disp("Turn in stage 2")
+yalmip('clear')
+x = sdpvar(2,N+1); % [x; y]
+u = sdpvar(2,N+1); % [v1; v2]
+eps_h = sdpvar(1);
+cost = lambda1*(x(1,end)^2 + x(2,end)^2) + lambda2 * (1-cos(psi_d)*u(1,end)/Vhs(end) - sin(psi_d)*u(2,end)/Vhs(end)) + lambda3*eps_h;
+constraints = [x(:,1) == [x0;y0], ...
+               u(:,1) == u0, ...
+               norm(u(:,N+1)) <= Vhs(N+1) + eps_h, eps_h>=0];
+for i = 1:N
+    constraints = [constraints, x(:,i+1) == x(:,i) + dt*[(u(1,i)+u(1,i+1))/2 + (Ws(1,i)+Ws(1,i+1))/2;
+                                                         (u(2,i)+u(2,i+1))/2 + (Ws(2,i)+Ws(2,i+1))/2] ...
+                              , norm(u(:,i)) <= Vhs(i) + eps_h ...
+                              , norm(u(:,i+1) - u(:,i))/Vhs(i)/dt <= psi_dot_m];
+    cost = cost + lambda3*norm(u(:,i+1) - u(:,i))^2/Vhs(i)^2/dt;
+end
+options = sdpsettings('verbose',0,'solver','ecos');
+
+figure(2)
+hold on
+grid on
+
+for n = 1:MAX_ITER
+    Cons = constraints;
+    for i = 1:N+1
+        Cons = [Cons, u_bar_normalized(:,i)'*u(:,i) >= Vhs(i) - eps_h];
+    end
+    sol = optimize(Cons, cost, options);
+    if sol.problem ~= 1
+        if abs(value(cost) - current_cost) < eps_convergence*0.01
+            xs = value(x);
+            us = value(u);
+            for j = 1:N+1
+                u_bar_normalized(:,j) = us(:,j)/norm(us(:,j));
+            end
+%             guidance(1:2,:) = xs(:,2:end);
+%             guidance(3,:) = hs(2:end);
+%             guidance(4,:) = atan2(us(2,2:end), us(1,2:end));
+%             guidance(5,:) = 
+% 
+%             us = value(u);
+%             flag = true;
+
+            disp("At iteration "+num2str(n)+", final cost: "+num2str(value(cost)))
+            disp([lambda1*(xs(1,end)^2 + xs(2,end)^2), lambda2 * (1-cos(psi_d)*us(1,end)/Vhs(end) - sin(psi_d)*us(2,end)/Vhs(end))])
+            plot(xs(2,:), xs(1,:),'r')
+            break
+        else
+            current_cost = value(cost);
+            disp("At iteration "+num2str(n)+", current cost: "+num2str(current_cost))
+            xs = value(x);
+            plot(xs(2,:), xs(1,:),'k')
+            for j = 1:N+1
+                us = value(u);
+                u_bar_normalized(:,j) = us(:,j)/norm(us(:,j));
+            end
+        end
+    else
+        disp("###############################################")
+        disp("[ERROR] Something went wrong at iteration "+num2str(n));
+        disp(sol.info)
+        disp("###############################################")
+    end
+end 
+
+end
