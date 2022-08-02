e2622bfe2011a7589f949194e21db3dbec8cd2c4
yuliangzhong <yubaby2003@163.com>
2022-08-02 19:40:06 +0200

    test done

--- GuidanceSolve.m
+++ GuidanceSolve.m
@@ -1,7 +1,7 @@
 % solving guidance by casadi + ipopt
 % with safety constraints
 
-function [flag, guidance] = GuidanceSolve(N, psi_d, vel_info, psi_dot_m, psi_ddot_m, init_cond, psi_dot_now, heights, wind_profile_hat, wind_dis, Axbxh, guidance_guess)
+function [flag, guidance] = GuidanceSolve(N, vel_info, psi_dot_m, psi_ddot_m, init_cond, psi_dot_now)
 
 tic
 
@@ -13,28 +13,13 @@ psi_0 = init_cond(4);
 
 Vh = vel_info(2);
 Vz = vel_info(3);
-tf = h0/(Vz+wind_dis(3)); % wind error z
+tf = h0/Vz;
 dt = tf/(N-1);
 hs = linspace(h0, 0, N);
-
-Ws = [interp1(heights, wind_profile_hat(1,:), hs, 'linear','extrap');
-      interp1(heights, wind_profile_hat(2,:), hs, 'linear','extrap')];
-
-id_p = floor(N*0.2); % 20% steps
-hp = hs(id_p);
-ratios = exp(-5*(h0*ones(1,N)-hs)/(h0-hp));
-Ws = Ws + [wind_dis(1); wind_dis(2)] .* ratios; % wind error xy
-
-%% Compute Safezone Constraints
-inds = zeros(size(Axbxh,1),1); % turn h to index
-for i = 1:size(Axbxh,1)
-    inds(i) = find(hs<Axbxh(i,4),1);
-end
 
 %% Optimization Start
 
 lambda1 = 100;
-lambda2 = 100;
 
 Au = [1; -1];
 bu = [psi_dot_m; psi_dot_m];
@@ -43,27 +28,27 @@ Prob = casadi.Opti();
 x = Prob.variable(3, N);
 u = Prob.variable(1, N);
 
-% set initial guess from guidance guess
-if sum(guidance_guess,'all') == 0
-    disp("WARNING! Solving guidance without initial guess!!")
-else
-    x_guess = [x0; y0; psi_0]*ones(1,N);
-    u_guess = psi_dot_now*ones(1,N);
-    for i = 1:N-1
-        u_last = interp1(guidance_guess(3,:),guidance_guess(5,:),hs(i),'spline', 'extrap');
-        % u_guess should satisfy constraints!
-        u_guess(i+1) = max([min([u_last, psi_dot_m, u_guess(i)+psi_ddot_m*dt]), -psi_dot_m, u_guess(i)-psi_ddot_m*dt]);
-        % compute x_guess from forward Euler
-        x_guess(:,i+1) = x_guess(:,i) + dt*[(Vh*cos(x_guess(3,i))+Vh*cos(x_guess(3,i+1)))/2 + (Ws(1,i)+Ws(1,i+1))/2;
-                                            (Vh*sin(x_guess(3,i))+Vh*sin(x_guess(3,i+1)))/2 + (Ws(2,i)+Ws(2,i+1))/2;
-                                            u_guess(i)];
-    end
-    Prob.set_initial(x, x_guess);
-    Prob.set_initial(u, u_guess);
-end
+% % set initial guess from guidance guess
+% if sum(guidance_guess,'all') == 0
+%     disp("WARNING! Solving guidance without initial guess!!")
+% else
+%     x_guess = [x0; y0; psi_0]*ones(1,N);
+%     u_guess = psi_dot_now*ones(1,N);
+%     for i = 1:N-1
+%         u_last = interp1(guidance_guess(3,:),guidance_guess(5,:),hs(i),'spline', 'extrap');
+%         % u_guess should satisfy constraints!
+%         u_guess(i+1) = max([min([u_last, psi_dot_m, u_guess(i)+psi_ddot_m*dt]), -psi_dot_m, u_guess(i)-psi_ddot_m*dt]);
+%         % compute x_guess from forward Euler
+%         x_guess(:,i+1) = x_guess(:,i) + dt*[(Vh*cos(x_guess(3,i))+Vh*cos(x_guess(3,i+1)))/2 + (Ws(1,i)+Ws(1,i+1))/2;
+%                                             (Vh*sin(x_guess(3,i))+Vh*sin(x_guess(3,i+1)))/2 + (Ws(2,i)+Ws(2,i+1))/2;
+%                                             u_guess(i)];
+%     end
+%     Prob.set_initial(x, x_guess);
+%     Prob.set_initial(u, u_guess);
+% end
 
 % costs and constraints
-cost = lambda1*(x(1,end)^2 + x(2,end)^2) + lambda2*(1-cos(x(3,end)-psi_d));
+cost = lambda1*(x(1,end)^2 + x(2,end)^2);
 
 % x1
 Prob.subject_to(x(:,1) == [x0; y0; psi_0]);
@@ -72,8 +57,8 @@ Prob.subject_to(u(1) == psi_dot_now);
 
 for i = 1:N-1
     % x2~xN
-    Prob.subject_to(x(:,i+1) == x(:,i) + dt*[(Vh*cos(x(3,i))+Vh*cos(x(3,i+1)))/2 + (Ws(1,i)+Ws(1,i+1))/2;
-                                             (Vh*sin(x(3,i))+Vh*sin(x(3,i+1)))/2 + (Ws(2,i)+Ws(2,i+1))/2;
+    Prob.subject_to(x(:,i+1) == x(:,i) + dt*[(Vh*cos(x(3,i))+Vh*cos(x(3,i+1)))/2;
+                                             (Vh*sin(x(3,i))+Vh*sin(x(3,i+1)))/2;
                                              u(i)]);
     % u2~uN
     cost = cost + u(i+1)^2*dt;
@@ -81,14 +66,6 @@ for i = 1:N-1
     % u2-u1 ~ uN-uN-1
     Prob.subject_to(u(i+1)-u(i) <= dt*psi_ddot_m)
     Prob.subject_to(u(i+1)-u(i) >= -dt*psi_ddot_m) % control input time derivitive constraints
-end
-
-% constraints of safezone
-for j = 1:size(Axbxh,1)
-    % x_ind ~ xN
-    for k = inds(j):N
-        Prob.subject_to(Axbxh(j,1:2)*x(1:2,k) <= Axbxh(j,3));
-    end
 end
 
 Prob.minimize(cost)
@@ -102,17 +79,17 @@ try
     us = sol.value(u);
 
     pos_cost = lambda1*(xs(1,end)^2 + xs(2,end)^2);
-    if pos_cost>400
-        flag = false;
-        guidance = zeros(5, N);
-        disp("reject bad guidance!")
-    else
+%     if pos_cost>400
+%         flag = false;
+%         guidance = zeros(5, N);
+%         disp("reject bad guidance!")
+%     else
         flag = true;
         guidance(1:2,1:N) = xs(1:2,1:N);
         guidance(3,1:N) = hs(1:N);
         guidance(4,1:N) = xs(3,1:N);
         guidance(5,1:N) = us(1:N);
-    end
+%     end
 
 catch
     disp("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
--- VhVzFit.m
+++ VhVzFit.m
@@ -6,36 +6,41 @@ xs = reshape(out.position.signals.values(1,1,:),[],1);
 ys = reshape(out.position.signals.values(2,1,:),[],1);
 zs = reshape(out.position.signals.values(3,1,:),[],1);
 
-% N = size(xs,1);
-% t_end = out.position.time(end);
-% Ts = linspace(0, t_end, N)';
+fit_linear = 0;
 
-% X = polyfit(Ts,xs,1);
-% Xfit = X(1)*Ts + X(2);
-% Y = polyfit(Ts,ys,1);
-% Yfit = Y(1)*Ts + Y(2);
-% Z = polyfit(Ts,zs,1);
-% Zfit = Z(1)*Ts + Z(2);
-% 
-% hold on
-% scatter(Ts, xs, 5, 'filled')
-% scatter(Ts, ys, 5, 'filled')
-% scatter(Ts, zs, 5, 'filled')
-% plot(Ts,Xfit);
-% plot(Ts,Yfit);
-% plot(Ts,Zfit);
-% 
-% grid on
-% 
-% disp([X(1), Y(1), sqrt(X(1)^2+Y(1)^2), Z(1)])
-a = [xs, ys, ones(size(xs))]\(-(xs.^2+ys.^2));
-xc = -a(1)/2;
-yc = -a(2)/2;
-R = sqrt((a(1)^2+a(2)^2)/4-a(3));
-thetas = linspace(0, 2*pi, 100);
-xt = xc+R*cos(thetas);
-yt = yc+R*sin(thetas);
-hold on
-plot(xt, yt)
-scatter(xs, ys, 10, 'filled')
-axis equal
+if fit_linear
+    N = size(xs,1);
+    t_end = out.position.time(end);
+    Ts = linspace(0, t_end, N)';
+    
+    X = polyfit(Ts,xs,1);
+    Xfit = X(1)*Ts + X(2);
+    Y = polyfit(Ts,ys,1);
+    Yfit = Y(1)*Ts + Y(2);
+    Z = polyfit(Ts,zs,1);
+    Zfit = Z(1)*Ts + Z(2);
+    
+    hold on
+    scatter(Ts, xs, 5, 'filled')
+    scatter(Ts, ys, 5, 'filled')
+    scatter(Ts, zs, 5, 'filled')
+    plot(Ts,Xfit,'--','LineWidth',5);
+    plot(Ts,Yfit,'--','LineWidth',5);
+    plot(Ts,Zfit,'--','LineWidth',5);
+    
+    grid on
+    
+    disp([X(1), Y(1), sqrt(X(1)^2+Y(1)^2), Z(1)])
+else
+    a = [xs, ys, ones(size(xs))]\(-(xs.^2+ys.^2));
+    xc = -a(1)/2;
+    yc = -a(2)/2;
+    R = sqrt((a(1)^2+a(2)^2)/4-a(3));
+    thetas = linspace(0, 2*pi, 100);
+    xt = xc+R*cos(thetas);
+    yt = yc+R*sin(thetas);
+    hold on
+    plot(xt, yt,'--','LineWidth',5)
+    scatter(xs, ys, 10, 'filled')
+    axis equal
+end
