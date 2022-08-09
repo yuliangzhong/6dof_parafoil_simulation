fa2a8034c11e47d52b98d59cf27619f0883d6998
yuliangzhong <yubaby2003@163.com>
2022-08-09 19:02:35 +0200

    4-dof ekf

--- ParamSetup.m
+++ ParamSetup.m
@@ -34,7 +34,7 @@ max_disturbance_norm = max(vecnorm(delta_ws(1:2,:)))
 disp(num2str(max_disturbance_norm/(wind_gust_max-vel_at6m)*100) + "%");
 disp("======================================")
 
-ifPlotWindInfo = 1;
+ifPlotWindInfo = 0;
 
 wind_err0 = zeros(4,50); % [h, dx, dy, dz] wind error = est_wind - wind_pf, stored in a queue
 
@@ -93,8 +93,8 @@ cM = [c_lp; c_lda; c_m0; c_ma; c_mq; c_nr; c_nda]; % moment coefficients
 
 %% Safe Zone, Vel Info and Initiation
 [Axbxh, init_xy_pos] = SafeZoneCompute(0);
-init_pos_in_inertial_frame = [init_xy_pos; -50]; % x-North, z-down, y-East
-init_rpy = [0; 0.006; -10/180*pi]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
+init_pos_in_inertial_frame = [0;0; -100]; % x-North, z-down, y-East
+init_rpy = [0; 0.006; 155/180*pi]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
 init_uvw = [3.819; -0.673; 1.62]; % velocity in body frame % shouldn't be all zero
 init_pqr = [0; 0; 0]; % angular velocity in body frame
 
@@ -107,8 +107,8 @@ psi_dot_m = 0.1906; % maximum turning angular vel without wind [rad/s]
 delta_dot_m = 0.5; % [/s]
 
 %% Sensor Model: Accuracy after Primary Sensor Fusion
-sensor_freq = 100; % [Hz] sensor data subscriber frequency
-pos_accu = 3; % [m]
+sensor_freq = 10; % [Hz] sensor data subscriber frequency
+pos_accu = 1.5; % [m]
 acc_accu = 0.2; % [m/s^2]
 angVel_accu = 1; % [deg/s]
 
@@ -118,12 +118,24 @@ yaw_accu = 0.5; % [degree]
 
 %% Extended Kalman Filter for States
 % state X = [x, y, z, x_dot, y_dot, z_dot, row, pitch, yaw] 9*1
+% EKF_freq = sensor_freq; % [Hz]
+% % state_mu0 = [init_pos_in_inertial_frame; 
+% %              GroundSpeedCompute(init_rpy, init_uvw); 
+% %              init_rpy]; % 9*1
+% % state_mu0 = [init_pos_in_inertial_frame;rand();rand();-abs(rand());rand();rand();rand()];
+% % state_mu0 = [init_pos_in_inertial_frame;rand();rand();-abs(rand());0;0;rand()];
+% 
+% state_mu0 = [init_pos_in_inertial_frame;zeros(6,1)];
+% state_sigma0 = blkdiag(10*eye(3), 1*eye(3), 1*eye(3)); % 9*9
+% Q = blkdiag(acc_accu^2*eye(3), (angVel_accu/180*pi)^2*eye(3)); % 6*6
+% R = pos_accu^2*eye(3); % 3*3
+
+%% 4-dof Extended Kalman Filter for States
+% state X = [x, y, z, x_dot, y_dot, z_dot, yaw] 7*1
 EKF_freq = sensor_freq; % [Hz]
-state_mu0 = [init_pos_in_inertial_frame; 
-             GroundSpeedCompute(init_rpy, init_uvw); 
-             init_rpy]; % 9*1
-state_sigma0 = blkdiag(4*eye(3), 2*eye(3), 0.4*eye(3)); % 9*9
-Q = blkdiag(acc_accu^2*eye(3), (angVel_accu/180*pi)^2*eye(3)); % 6*6
+state_mu0 = [init_pos_in_inertial_frame+[rand();rand();rand()];zeros(4,1)];
+state_sigma0 = blkdiag(5*eye(3), 3*eye(3), 1); % 7*7
+Q = blkdiag(acc_accu^2*eye(3), (angVel_accu/180*pi)^2); % 4*4
 R = pos_accu^2*eye(3); % 3*3
 
 %% Guidance
