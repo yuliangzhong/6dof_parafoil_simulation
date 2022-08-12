24558002d192cd608944499331f779acdaf354f1
yuliangzhong <yubaby2003@163.com>
2022-08-12 09:46:05 +0200

    6dof rigid body kinematics ekf

--- ParamSetup.m
+++ ParamSetup.m
@@ -34,7 +34,7 @@ max_disturbance_norm = max(vecnorm(delta_ws(1:2,:)))
 disp(num2str(max_disturbance_norm/(wind_gust_max-vel_at6m)*100) + "%");
 disp("======================================")
 
-ifPlotWindInfo = 1;
+ifPlotWindInfo = 0;
 
 wind_err0 = zeros(4,50); % [h, dx, dy, dz] wind error = est_wind - wind_pf, stored in a queue
 
@@ -93,8 +93,8 @@ cM = [c_lp; c_lda; c_m0; c_ma; c_mq; c_nr; c_nda]; % moment coefficients
 
 %% Safe Zone, Vel Info and Initiation
 [Axbxh, init_xy_pos] = SafeZoneCompute(0);
-init_pos_in_inertial_frame = [init_xy_pos; -100]; % x-North, z-down, y-East
-init_rpy = [0; 0.006; -10/180*pi]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
+init_pos_in_inertial_frame = [0;0; -150]; % x-North, z-down, y-East
+init_rpy = [0; 0.006; -150/180*pi]; % yaw-pitch-row; from ground to body frame; x-head, z-done, y-right
 init_uvw = [3.819; -0.673; 1.62]; % velocity in body frame % shouldn't be all zero
 init_pqr = [0; 0; 0]; % angular velocity in body frame
 
@@ -107,40 +107,42 @@ psi_dot_m = 0.1906; % maximum turning angular vel without wind [rad/s]
 delta_dot_m = 0.5; % [/s]
 
 %% Sensor Model: Accuracy after Primary Sensor Fusion
-sensor_freq = 20; % [Hz] sensor data subscriber frequency
-pos_accu = 2; % [m]
+sensor_freq = 10; % [Hz] sensor data subscriber frequency
+pos_accu = 3; % [m]
+
 vel_accu = 0.1; % [m/s]
 row_pitch_accu = 0.1; % [degree]
 yaw_accu = 0.5; % [degree]
-acc_accu = 0.1; % [m/s^2]
-angVel_accu = 0.1; % [deg/s]
+
+acc_accu = 0.2; % [m/s^2]
+angVel_accu = 1; % [deg/s]
 % should tune white noise power in simulator for accuracy of airspeed, AOA, and AOS
 
 %% Extended Kalman Filter for States
 % state X = [x, y, z, x_dot, y_dot, z_dot, row, pitch, yaw] 9*1
 EKF_freq = sensor_freq; % [Hz]
-state_mu0 = [init_pos_in_inertial_frame; 
-             GroundSpeedCompute(init_rpy, init_uvw); 
-             init_rpy]; % 9*1
+% state_mu0 = [init_pos_in_inertial_frame; 
+%              GroundSpeedCompute(init_rpy, init_uvw); 
+%              init_rpy]; % 9*1
+state_mu0 = [init_pos_in_inertial_frame; zeros(6,1)]; % 9*1
 state_sigma0 = blkdiag(4*eye(3), 2*eye(3), 0.4*eye(3)); % 9*9
 Q = blkdiag(acc_accu^2*eye(3), (angVel_accu/180*pi)^2*eye(3)); % 6*6
-R = blkdiag(pos_accu^2*eye(3), vel_accu^2*eye(3), ...
-            diag([(row_pitch_accu/180*pi)^2, (row_pitch_accu/180*pi)^2, (yaw_accu/180*pi)^2])); % 9*9
+R = pos_accu^2*eye(3); % 3*3
 
-%% Guidance
-psi_d = theta/180*pi; % desired landing orientation: opposite to wind direction
-
-guidance_horizon_N = 100;
-guidance0 = GuidanceGuess(guidance_horizon_N, init_pos_in_inertial_frame);
-psi_ddot_m = psi_dot_m*2*delta_dot_m; % [rad/s2]
-
-%% MPCC Tracker
-time_horizon_N = 20; % < 50
-mpcc_freq = 1; % [Hz]
-mpcc_Ts = 0.5; % [s]
-mpcc_ctrl_freq = 10; % [Hz]
-control0 = zeros(3, time_horizon_N); % [h, psi, psi_dot]
-warning('off','MATLAB:polyfit:RepeatedPointsOrRescale') % suppress fit warnings in mpcc
+% %% Guidance
+% psi_d = theta/180*pi; % desired landing orientation: opposite to wind direction
+% 
+% guidance_horizon_N = 100;
+% guidance0 = GuidanceGuess(guidance_horizon_N, init_pos_in_inertial_frame);
+% psi_ddot_m = psi_dot_m*2*delta_dot_m; % [rad/s2]
+% 
+% %% MPCC Tracker
+% time_horizon_N = 20; % < 50
+% mpcc_freq = 1; % [Hz]
+% mpcc_Ts = 0.5; % [s]
+% mpcc_ctrl_freq = 10; % [Hz]
+% control0 = zeros(3, time_horizon_N); % [h, psi, psi_dot]
+% warning('off','MATLAB:polyfit:RepeatedPointsOrRescale') % suppress fit warnings in mpcc
 
 % %% Motor Model
 % % delta_s: 2nd order response
